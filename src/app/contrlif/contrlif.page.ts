09cec4f2212af1a9965705be069cb5fd066fdd64
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-07-27 15:23:12 +0700

    if statement

--- src/app/contrlif/contrlif.page.ts
+++ src/app/contrlif/contrlif.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contrlif.page.scss'],
 })
 export class ContrlifPage implements OnInit {
+  num1: number;
+  show: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  gotoResult() {
+    if (this.num1 > 50) {
+      this.show = 'pass';
+    }
+    if (this.num1 < 50) {
+      this.show = 'fail';
+    }
   }
-
 }
